2033d97166e731f415f4bfd1c8950d474434e87d
Austin Merrick <austinmerrick@onsclom.net>
2024-02-27 22:47:12 -0800

    Add day1 pt1

--- 1/main.c
+++ 1/main.c
@@ -1,6 +1,18 @@
 #include "stdio.h"
 
 int main() {
-  printf("Hello, World!\n");
-  return 0;
+  char c;
+  int sum = 0;
+  char first, last = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == '\n') {
+      sum += (first - '0') * 10 + (last - '0');
+      first = last = 0;
+    }
+    if (c >= '0' && c <= '9') {
+      first = first == 0 ? c : first;
+      last = c;
+    }
+  }
+  printf("part1: %d\n", sum);
 }
